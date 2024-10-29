9e9d2790c38626bfbabcd5a320adfa15f62dde1b
Boopathi_DataNostradamus <51164330+rajboopathiking@users.noreply.github.com>
2024-10-29 11:40:09 +0530

    Update app.py

--- API/main/app.py
+++ API/main/app.py
@@ -73,4 +73,4 @@ async def predict(image: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail="Prediction error")
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
